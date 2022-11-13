d493b59dad6c8e5c91dee4759d89be3d5ec7beb3
Madilena <madilenam@gmail.com>
2022-11-13 09:33:00 -0800

    meas

--- NC23Measurement/Measurement.cpp
+++ NC23Measurement/Measurement.cpp
@@ -1,77 +1,77 @@
 #include "Measurement.h"
 using namespace std;
 
-Measurement::Measurement(Measurement &m){
-    feet = &m.feet;
-    inches = &m.inches;
+Measurement::Measurement(Measurement &m) {
+    feet = m.feet;
+    inches = m.inches;
 }
-Measurement::Measurement(){}
-        
-Measurement::Measurement(unsigned int feet, unsigned int inches){
+Measurement::Measurement() {}
+
+Measurement::Measurement(unsigned int feet, unsigned int inches) {
     this->feet = feet;
     this->inches = inches;
 }
-        
+
 unsigned int Measurement::getFeet() const {
-    return this->feet;
+    return feet;
 }
-        
-unsigned int Measurement::getInches() const{
-     return this->inches;   
+
+unsigned int Measurement::getInches() const {
+    return inches;
 }
-        
-bool Measurement::setFeet(unsigned int feet){
+
+bool Measurement::setFeet(unsigned int feet) {
     this->feet = feet;
     return true;
 }
-        
-bool Measurement::setInches(unsigned int inches){
-    if(inches > 12){
+
+bool Measurement::setInches(unsigned int inches) {
+    if(inches > 12) {
         return false;
     }
     this->inches = inches;
     return true;
 }
-        
-void Measurement::operator++(int){
+
+void Measurement::operator++(int) {
     bool incrementFeet = false;
-    if(inches == 11){
+    if(inches == 11) {
         setInches(0);
         incrementFeet = true;
-    }else{
+    } else {
         setInches(getInches()+1);
     }
-    if(incrementFeet){
+    if(incrementFeet) {
         setFeet(getFeet()+1);
     }
 }
 
-void Measurement::operator--(int){
+void Measurement::operator--(int) {
     bool decrementFeet = false;
-    if(inches == 0){
+    if(inches == 0) {
         setInches(11);
         decrementFeet = true;
-    }else{
+    } else {
         setInches(getInches()+1);
     }
-    if(decrementFeet){
+    if(decrementFeet) {
         setFeet(getFeet() - 1);
     }
 }
-        
-Measurement Measurement::operator+(Measurement rhs){
+
+Measurement Measurement::operator+(Measurement rhs) {
     return *this;
 }
-        
+
 Measurement Measurement::operator-(Measurement rhs) {
     return *this;
 }
-        
-ostream& operator<<(ostream& out, const Measurement& t){
+
+ostream& operator<<(ostream& out, const Measurement& t) {
     return out;
 }
-        
-bool operator==(Measurement a, Measurement b){
+
+bool operator==(Measurement a, Measurement b) {
     return false;
 }
 
--- NC23Measurement/main.cpp
+++ NC23Measurement/main.cpp
@@ -6,7 +6,15 @@ int main() {
     cout << "Create a measurement m1 with 5 feet and 18 inches" << endl;
     Measurement m1(5, 18);
     Measurement m2 = m1;
-    cout << "m1: " << m1 << endl;
-    cout << "m2: " << m2 << endl;
+    cout << "m1: Feet=" << m1.getFeet() << " Inches=" << m1.getInches() << endl;
+    cout << "m2: Feet=" << m2.getFeet() << " Inches=" << m2.getInches() << endl;
 
+    if (m1 == m2) {
+        cout << "Measurement objects are equal" << endl;
+    } else {
+        cout << "Measurement objects are NOT equal" << endl;
+    }
+    cout << "Postfix increment m1 and display to console" << endl;
+    m1++;
+    cout << "m1: Feet=" << m1.getFeet() << " Inches=" << m1.getInches() << endl;
 }
